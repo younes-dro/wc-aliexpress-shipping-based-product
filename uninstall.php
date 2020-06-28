e64a41d74c8397439ee8a8eb8a258d97f0def9e5
younes-dro <dro66_8@hotmail.fr>
2020-06-28 06:56:18 +0100

    Add uninstall file to delete the option

--- uninstall.php
+++ uninstall.php
@@ -3,14 +3,10 @@
  * Uninstall plugin
  */
 
-// If uninstall not called from WordPress exit
+
 if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-//global $wpdb;
-//
-////change to standard select type custom attributes
-//$table = $wpdb->prefix . 'woocommerce_attribute_taxonomies';
-//$update = "UPDATE `$table` SET `attribute_type` = 'select' WHERE `attribute_type` != 'text'";
-//$wpdb->query( $update );
+// Delete option from options table
+delete_option( 'dro_shipping_options' );
